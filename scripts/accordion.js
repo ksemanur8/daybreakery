34f96dc2caad9df87b4097e7eb7fc0674c38bf36
Andrew Sipes <andrew.n.sipes@gmail.com>
2021-04-18 12:27:19 -0400

    Formatted with Prettier.

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -1,7 +1,7 @@
 var accordion = document.getElementsByClassName("accordion");
 
 for (let i = 0; i < accordion.length; i++) {
-  accordion[i].addEventListener("click", function() {
+  accordion[i].addEventListener("click", function () {
     this.classList.toggle("active");
 
     var panel = this.nextElementSibling;
